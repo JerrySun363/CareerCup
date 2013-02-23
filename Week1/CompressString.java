757c88063dab892bf05492e0985741f4d2778f10
Sun Chen <sunchen363@gmail.com>
2013-02-23 23:36:20 +0800

    1.5 and 1.6 solved

--- Week1/CompressString.java
+++ Week1/CompressString.java
@@ -1,3 +1,4 @@
+
 /*Implement a method to perform basic string compression using the counts of repeated characters. 
  * For example, the string aabcccccaaa would become a2b1c5a3. 
  * If the "compressed" string would not become smaller than the original string, 
@@ -5,10 +6,47 @@
  */
 
 public class CompressString {
-      public String compressString(String str){
-    	  
-    	  
-    	  
-    	  return null;
+	  public static String compressString(String str){
+		 String output="";
+		 char[] strArray=str.toCharArray();
+		 int startIndex = 0,endIndex=0;
+		 /* while(endIndex <=strArray.length){
+             if(endIndex==strArray.length||strArray[startIndex]!=strArray[endIndex]){
+            	 int tempLen=endIndex-startIndex;
+            	 output=output.concat(""+strArray[startIndex]+tempLen);
+            	 startIndex=endIndex;
+            	 if(endIndex==strArray.length)
+            		 break;
+              }else{
+            	 endIndex++; 
+             }
+		 }
+		 想了一下，for 循环在结尾部分的处理应该比while要好看*/
+		 /*while 循环略丑代码...*/
+		 
+		 for(endIndex=0;endIndex<strArray.length;endIndex++){
+			 if(strArray[startIndex]!=strArray[endIndex]){
+				 int tempLen=endIndex-startIndex;
+            	 output=output.concat(""+strArray[startIndex]+tempLen);
+            	 startIndex=endIndex; 
+			 }
+		 }
+		
+		 /*Final group is not easy to be merged
+		  * calculate it separately.*/
+		 int tempLen=endIndex-startIndex;
+    	 output=output.concat(""+strArray[startIndex]+tempLen);
+		 
+		 if(output.length()>str.length())
+			 return str;
+		 
+         return output; 
       }
+	  
+	  public static void main(String args[]){
+		  System.out.println(compressString("abcdeffffffffeeeeeettttteeeee"));
+		  
+	  }
+	  
 }
+
---
+++ Week1/RotateImage.java
@@ -0,0 +1,47 @@
+/*Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes 
+ * write a method to rotate the image by 90 degrees. 
+ * Can you do this in place?
+ */
+public class RotateImage {
+	/*Since an integer is composed of four bytes, and the pixels' order are not changed in a single pixel
+	 * it is reasonable to store in integers
+	 *  To be in place, first swap the two rows in the same column, the middle row unchanged
+	 *  Then swap the two according to the Diagonal as the axis of symmetry*/
+	
+	public static int[][] rotateImage(int[][] image){
+		int n=image.length;
+		/*The first step*/
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n/2;j++){
+			image[i][j]=image[i][j]+image[i][n-j-1];
+			image[i][n-j-1]=image[i][j]-image[i][n-j-1];
+			image[i][j]=image[i][j]-image[i][n-j-1];
+			}
+		}
+		/*The second step*/
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n-i-1;j++){
+			image[i][j]=image[i][j]+image[n-j-1][n-i-1];
+			image[n-j-1][n-i-1]=image[i][j]-image[n-j-1][n-i-1];
+			image[i][j]=image[i][j]-image[n-j-1][n-i-1];
+			}
+		}
+		
+		return image;
+	}
+	
+    /*public static void swap(int a, int b){
+    	a=a+b;
+    	b=a-b;
+    	a=a-b;
+      }*/
+    
+    public static void main(String args[]){
+    int[][] image={{1,2,3},{4,5,6},{7,8,9}};
+    image=rotateImage(image);
+    System.out.println(image[0][0]+" "+image[0][1]+" "+image[0][2]+" \r"+image[1][0]+" "+image[1][1]+" "+image[1][2]+" \r"+image[2][0]+" "+image[2][1]+" "+image[2][2]);
+    	
+    }
+    
+
+}
